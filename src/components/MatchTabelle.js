d7f2aaa0e11cd7715db0b1165b238c8f83377fc5
chris125964 <chris125964@yahoo.de>
2016-04-04 22:56:08 +0200

    MatchTabelle

---
+++ src/components/MatchTabelle.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+import Match from '../components/Match';
+
+import _ from 'underscore';
+
+class MatchTabelle extends React.Component {
+
+  	render() {
+    	return (
+      <div>
+      <Table >
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Team</th>
+            <th>Punkte</th>
+            <th>Tore</th>
+          </tr>
+        </thead>
+        <tbody>
+          { _.map(this.props.teams, (team, index) => <tr>
+              <td>{index + 1}</td>
+              <td>{team}</td>
+              <td>punkte</td>
+              <td>tore</td>
+            </tr>
+      ) }
+        </tbody>
+      </Table>
+      </div>
+      );
+  }
+}
+
+export default MatchTabelle;
